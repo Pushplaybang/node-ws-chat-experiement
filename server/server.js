26a83aaab1da543b26139b02e42953e44a54c51e
pushplaybang <pushplaybang@gmail.com>
2018-09-24 02:10:36 +0200

    very basic socket connection established

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log('new user connected');
 
-app.listen(3000, () => {
+  socket.on('disconnect', () => {
+    console.log('user was disconnected');
+  });
+});
+
+
+server.listen(3000, () => {
   console.log('server running on port 3000');
 });
